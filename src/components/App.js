c30f06917fbb4e1a5aec3fe91aa288e179c7d599
Shawn Sullivan <sullivansm3@gmail.com>
2020-02-20 10:09:34 -0700

    Display a list of movies from hardcoded data

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import '../main.css';
+
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 
 class App extends React.Component {
   render(){
     return(
     <div>
-      Starter App component - hello there
+      <div className="title">{movies[0].title}</div>
+      <div className="title">{movies[1].title}</div>
+      <div className="title">{movies[2].title}</div>
+      <div className="title">{movies[3].title}</div>
+      <div className="title">{movies[4].title}</div>
     </div>
   )}
 }
